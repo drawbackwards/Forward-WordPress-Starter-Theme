49fe2719fd27ede675ef519225546f0f0ebfed7b
Ben Leivian <ben@drawbackwards.com>
2015-04-09 22:04:26 +0000

    Initial project commit.

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Forward
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="container">
+			<div class="site-info">
+				<?php printf( __( 'Site by %1$s', 'forward' ), '<a href="http://drawbackwards.com" rel="designer">Drawbackwards</a>' ); ?>
+			</div><!-- .site-info -->
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Forward
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'forward' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="container">
+			<div class="site-branding">
+				<h1 class="site-title"><a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<div class="site-description"><?php bloginfo( 'description' ); ?></div>
+			</div><!-- .site-branding -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<div class="container">
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false ) ); ?>
+				</div>
+			</nav><!-- #site-navigation -->
+			<div id="mobile-menu-switch">
+				<a href="" class="toggle">Menu</a>
+			</div>
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
